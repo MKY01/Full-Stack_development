de458e9baa91f38d48ed30dfd25c293c86a3cbd1
mkydatascientist <myip01@mail.bbk.ac.uk>
2020-06-12 00:58:36 +0100

    refactor and update
    flask migration completed

--- practice/todoapp/app.py
+++ practice/todoapp/app.py
@@ -8,26 +8,29 @@ INSERT INTO todos (description) VALUES ('Do a thing 1');
 INSERT INTO todos (description) VALUES ('Do a thing 2');
 INSERT INTO todos (description) VALUES ('Do a thing 3');
 INSERT INTO todos (description) VALUES ('Do a thing 4');
+ALTER TABLE ADD COLUMN;
 '''
 
 
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost:5432/todoapp'
 db = SQLAlchemy(app)  #session_options={"expire_on_commit":False}
 
+migrate = Migrate(app, db)
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
-
-#db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -47,7 +50,7 @@ def create_todo():
         db.session.close()
     if error:
         abort (400)
-    else:       
+    else:
         return jsonify(body)
 
 @app.route('/')
