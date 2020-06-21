ce09e0e66b8e159a57a36118fc4baa27ecce23c6
mkydatascientist <myip01@mail.bbk.ac.uk>
2020-06-21 23:50:25 +0100

    update model
    1-Many relationship defined

--- practice/todoapp/app.py
+++ practice/todoapp/app.py
@@ -19,10 +19,17 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=True)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
 
 # By convention, we would write a POST endpoint to /todos for the create endpoint:@app.route('/todos', method=['POST'])
 @app.route('/todos/create', methods=['POST'])
