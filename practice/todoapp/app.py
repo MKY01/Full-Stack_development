a08a61ba0b8755915413c857f49b2e7196b8573f
mkydatascientist <myip01@mail.bbk.ac.uk>
2020-06-24 23:48:10 +0100

    update lists of todos
    select different lists

--- practice/todoapp/app.py
+++ practice/todoapp/app.py
@@ -84,6 +84,14 @@ def delete_todo(todo_id):
     #on success, it should return to the index (/) route
     return jsonify({'success': True})
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html',
+    data=Todo.query.filter_by(list_id=list_id).order_by('id')
+    .all()
+    )
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', todos=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
