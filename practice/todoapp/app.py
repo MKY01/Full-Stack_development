7e0c90e794becc2244cf373b7c09c23ef5b19e70
mkydatascientist <myip01@mail.bbk.ac.uk>
2020-06-25 00:39:40 +0100

    todoapp crud lists toggle

--- practice/todoapp/app.py
+++ practice/todoapp/app.py
@@ -87,10 +87,11 @@ def delete_todo(todo_id):
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
     return render_template('index.html',
-    data=Todo.query.filter_by(list_id=list_id).order_by('id')
+    lists=TodoList.query.all(),
+    active_list=TodoList.query.get(list_id),
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id')
     .all()
-    )
-
+)
 
 @app.route('/')
 def index():
